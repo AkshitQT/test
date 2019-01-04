6e8d148f193685bc017acc4fed6a24b8885d4e3d
AkshitQT <34470027+AkshitQT@users.noreply.github.com>
2019-01-04 19:06:26 +0530

    Add files via upload

---
+++ jenkins/src/test/java/Test_Jenkins/jenkins/AppTest.java
@@ -0,0 +1,23 @@
+package Test_Jenkins.jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+		System.out.println();
+    	System.out.println("Jenkins test Job executed Successfully");
+		System.out.println();
+        assertTrue( true );
+    }
+}
